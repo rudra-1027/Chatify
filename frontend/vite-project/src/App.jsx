41f175ff3e77a9e6f0c528a426a5ba93a8f78ade
rudra-1027 <rudrakahar10@gmail.com>
2026-01-16 18:25:01 +0530

    Update App.jsx

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Routes, Route } from "react-router";
 import './App.css'
