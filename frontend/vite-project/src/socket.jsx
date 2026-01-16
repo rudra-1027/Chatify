c134fc88a1f15cfed032b8a9d7651e97c711c531
rudra-1027 <rudrakahar10@gmail.com>
2026-01-16 14:07:15 +0530

    Update socket.io to use production backend URL

--- frontend/vite-project/src/socket.jsx
+++ frontend/vite-project/src/socket.jsx
@@ -1,7 +1,7 @@
 import {io} from 'socket.io-client'
 
 
-const socket=io("http://localhost:5000", {
+const socket=io(import.meta.env.VITE_BACKEND_URL, {
 
   withCredentials:true,
   autoConnect: false
